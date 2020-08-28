ac77a2205a722db643c66bcca59f0ec3071ca0d4
ViacheslavSavitsky <70149273+ViacheslavSavitsky@users.noreply.github.com>
2020-08-28 16:50:46 +0300

    first commit
    first commit

---
+++ infotehTEST/infotehTEST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  infotehTEST
+//
+//  Created by Вячеслав on 28.08.2020.
+//  Copyright © 2020 Savitsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
